e9f9c8f82ec670875da2f000866a05f296e30865
lianweiqi <lianweiqi@qq.com>
2019-12-15 22:46:36 +0800

    20191215

--- numpy_fromfile.py
+++ numpy_fromfile.py
@@ -2,45 +2,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
 with open('3.ae', 'rb') as ae:
-    ae.seek(10, 0)
+    ae.seek(6, 0)
     A = np.fromfile(ae, '<u2', -1)
-    print(A)
-    B = A.reshape((-1, 3))
-    print(B)
-    # print(C)
-    a, b, c = np.hsplit(B, 3)
-    print(a,'\n', b,'\n', c)
+    # print(A)
+    B = A.reshape((-1, 4))
+    # print(B)
+    a, _, b, c = np.hsplit(B, 4)
+    # print(a,'\n', b,'\n', c)
+    a, b, c = a.astype('int32') - 32768, b.astype('int32') - 32768, c.astype('int32') - 32768
+    # print(a,'\n', b,'\n', c)
     
     plt.subplot(3, 1, 1)
-    plt.plot(20*np.log10(np.abs(a[:150])))
+    plt.plot(a[:150])
     plt.subplot(3, 1, 2)
-    plt.plot(20*np.log10(np.abs(b[:150])))
+    plt.plot(b[:150])
     plt.subplot(3, 1, 3)
-    plt.plot(20*np.log10(np.abs(c[:150])))
+    plt.plot(c[:150])
+    plt.figure(2)
+    plt.plot(a[:150])
+    plt.plot(b[:150])
+    plt.plot(c[:150])
 # %%
 print(a.shape)
 print(b.shape)
+print(c.shape)
 # %%
-import numpy as np
-import matplotlib.pyplot as plt
+# import numpy as np
+# import matplotlib.pyplot as plt
 with open('3.tev', 'rb') as tev:
     tev.seek(4, 0)
-    tev_data = np.fromfile(tev, '<u2', -1)
-    a = tev_data.astype('int32')
-    test_data = a - 32768
+    tev_data = np.fromfile(tev, '<u2', 25690112)
+    tev_data = tev_data.astype('int32') - 32768
     print(tev_data)
-    print(a)
-    print(test_data)
-    # plt.plot(tev_data)
-    # print(test_data)
-    # plt.plot(tev_data)
-    # print(test_data)
-    # plt.plot(test_data)
-    m = test_data[:150]
-    plt.plot(m)
-    plt.show()
+    plt.plot(tev_data[17250:17350])
 # %%
-print(tev_data)
 print(tev_data.shape)
 
 
